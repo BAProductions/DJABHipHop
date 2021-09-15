0dfee01a0669b5a9525ce9b8d30f88b0392ac48c
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-09-15 08:01:34 -0400

    DJABhipHop: Fade in

--- ui_fix.js
+++ ui_fix.js
@@ -3,7 +3,7 @@ $(function() {
   $({blurRadius: 0}).animate({blurRadius: 100}, {
     duration: 1000,
     step: function() {
-      $(".fa-wrapper").css({
+      $("#fa-wrapper").css({
         "opacity": this.blurRadius + "%"
       });
     }
